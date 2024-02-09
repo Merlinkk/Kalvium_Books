b9b915eef665f43b737a687e096614c2c9f9ab20
Merlinkk <anshulkashyap2504@gmail.com>
2024-02-09 09:51:58 +0530

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className=' w-full mt-auto h-14 bg-gray-50 bg-opacity-80'>
+    <footer className=' w-full mt-auto h-fit shadow-lg bg-gray-50 bg-opacity-80 bg-gradient-to-b from-red-50 to-red-300'>
         <div className='text-black p-4 flex  flex-col items-center justify-center md:text-xl text-base' >
             <div>Data fetched from- <a href='https://reactnd-books-api.udacity.com/books' className='text-blue-300'>Udacity</a></div>
             <p>By Anshul Kashyap</p>
